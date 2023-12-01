c9960e75cd18ae8084446fe7359a9b71faa8d9d2
Bilawal ali <bilawal.ali2626@gmail.com>
2023-12-01 05:24:23 -0800

    Add portfolio image

--- src/sections/home/testimonials/Testimonials.tsx
+++ src/sections/home/testimonials/Testimonials.tsx
@@ -2,9 +2,14 @@ import { Card } from "@/components/card/Card";
 import GeneralHeading from "@/components/Heading/GeneralHeading";
 import SubHeading from "@/components/Heading/SubHeading";
 import React from "react";
-import editableImage from "../../../../src/images/editable.svg";
-import langoImage from "../../../../src/images/lango.svg";
-import iFormatImage from "../../../../src/images/iFormat.svg";
+import KbrLogo from "../../../../src/images/kbr-logo.png";
+import WhiteKbrLogo from "../../../../src/images/-logokbr-white.png"
+import UcImage from "../../../../src/images/uc.png";
+import UcWhiteImage from "../../../../src/images/white-uc.png";
+import Aws from "../../../../src/images/aws.png";
+import AwsWhite from "../../../../src/images/awsblack.png";
+import Ks from "../../../../src/images/Kushal-White.png";
+import KsBlack from "../../../../src/images/Ks-black.png";
 import instructiveImage from "../../../../src/images/instructure.svg";
 import backgroundImage from "../../../../src/images/testomonial-n.png";
 import Image from "next/image";
@@ -19,82 +24,40 @@ import "swiper/css/scrollbar";
 const Testimonials = () => {
   const TESTIMONIALS_DATA = [
     {
-      image: editableImage,
-      text: "Globally actualize mission-critical services before client-centric action items. Rapidiously recaptiualize multifunctional bandwidth and resource maximizing ideas. Assertively customize interoperable ROI and granular experiences. Holisticly integrate accurate Globally actualize mission-critical services before client-centric action items.",
-      title: "Editable dm.com",
-      name: "Stella Alvarez",
+      whiteimage: WhiteKbrLogo,
+      image: KbrLogo,
+      text: "KBR Transport recognizes that in a constantly changing environment, only the companies that are  raising the bar for themselves are likely to survive in the market. By having our regular morning team huddles we discuss our customers transportation needs, obstacles, and frustrations. It is this management strategy that allows us to grasp a deep understanding of how each customer operates, their unique challenges and requirements, and how we can provide them with an array of added value transportation solutions along the way.",
+      title: "KBR Transport",
+      name: "Kapil Malik",
     },
     {
-      image: langoImage,
-      text: "Globally actualize mission-critical services before client-centric action items. Rapidiously recaptiualize multifunctional bandwidth and resource maximizing ideas. Assertively customize interoperable ROI and granular experiences. Holisticly integrate accurate Globally actualize mission-critical services before client-centric action items.",
-      title: "CEO Lango",
-      name: "Stella Alvarez",
+      whiteimage: UcWhiteImage,
+      image: UcImage,
+      text: "At Ultra Cleaning, we are dedicated to delivering top-quality cleaning services that leave your space spotless and refreshed. Our expert team uses advanced techniques and eco-friendly products to ensure the highest level of cleanliness. We take pride in our attention to detail and commitment to excellence. Discover the difference with Ultra Cleaning today",
+      title: "Ultra Cleaning",
+      name: "Waseem Afzal",
     },
     {
-      image: iFormatImage,
-      text: "Globally actualize mission-critical services before client-centric action items. Rapidiously recaptiualize multifunctional bandwidth and resource maximizing ideas. Assertively customize interoperable ROI and granular experiences. Holisticly integrate accurate Globally actualize mission-critical services before client-centric action items.",
-      title: "CEO iFormat",
-      name: "Stella Alvarez",
+      whiteimage: Aws,
+      image:  AwsWhite,
+      text: "At AWSRealEstate, we take pride in offering a comprehensive range of services tailored to meet the unique needs of both residential and commercial real estate throughout North America. From the charming landscapes of Whistler to the vibrant cityscape of Hope City, we are dedicated to providing top-tier real estate solutions. Our commitment is to ensure that your property goals not only meet but exceed your expectations, making them a reality in the diverse and dynamic North American market",
+      title: "AWS Real Estate ",
+      name: "Ashraf & Waseem & Sharry",
     },
     {
-      image: instructiveImage,
-      text: "Globally actualize mission-critical services before client-centric action items. Rapidiously recaptiualize multifunctional bandwidth and resource maximizing ideas. Assertively customize interoperable ROI and granular experiences. Holisticly integrate accurate Globally actualize mission-critical services before client-centric action items.",
-      title: "CEO otis",
-      name: "Stella Alvarez",
+    whiteimage: Ks,
+      image: KsBlack,
+      text: " We offer a range of legal services, all designed to help our clients. Whether you are looking for a small tweak or complete overhaul, we have you covered . We believe in a unique and customized consulting experience for each and every client. Each firm is unique. One-size-fits-all approaches will never help your problems reach a solution. We deliver custom solutions, tailored to you–your industry, your culture, your one-of-a-kind challenges.",
+      title: "Kaushal Law",
+      name: "Manmohan Kaushal",
     },
-    {
-      image: instructiveImage,
-      text: "Globally actualize mission-critical services before client-centric action items. Rapidiously recaptiualize multifunctional bandwidth and resource maximizing ideas. Assertively customize interoperable ROI and granular experiences. Holisticly integrate accurate Globally actualize mission-critical services before client-centric action items.",
-      title: "CEO otis",
-      name: "Stella Alvarez",
-    },
-    {
-      image: instructiveImage,
-      text: "Globally actualize mission-critical services before client-centric action items. Rapidiously recaptiualize multifunctional bandwidth and resource maximizing ideas. Assertively customize interoperable ROI and granular experiences. Holisticly integrate accurate Globally actualize mission-critical services before client-centric action items.",
-      title: "CEO otis",
-      name: "Stella Alvarez",
-    },
-    {
-      image: instructiveImage,
-      text: "Globally actualize mission-critical services before client-centric action items. Rapidiously recaptiualize multifunctional bandwidth and resource maximizing ideas. Assertively customize interoperable ROI and granular experiences. Holisticly integrate accurate Globally actualize mission-critical services before client-centric action items.",
-      title: "CEO otis",
-      name: "Stella Alvarez",
-    },
-    {
-      image: instructiveImage,
-      text: "Globally actualize mission-critical services before client-centric action items. Rapidiously recaptiualize multifunctional bandwidth and resource maximizing ideas. Assertively customize interoperable ROI and granular experiences. Holisticly integrate accurate Globally actualize mission-critical services before client-centric action items.",
-      title: "CEO otis",
-      name: "Stella Alvarez",
-    },
-    {
-      image: instructiveImage,
-      text: "Globally actualize mission-critical services before client-centric action items. Rapidiously recaptiualize multifunctional bandwidth and resource maximizing ideas. Assertively customize interoperable ROI and granular experiences. Holisticly integrate accurate Globally actualize mission-critical services before client-centric action items.",
-      title: "CEO otis",
-      name: "Stella Alvarez",
-    },
-    {
-      image: instructiveImage,
-      text: "Globally actualize mission-critical services before client-centric action items. Rapidiously recaptiualize multifunctional bandwidth and resource maximizing ideas. Assertively customize interoperable ROI and granular experiences. Holisticly integrate accurate Globally actualize mission-critical services before client-centric action items.",
-      title: "CEO otis",
-      name: "Stella Alvarez",
-    },
-    {
-      image: instructiveImage,
-      text: "Globally actualize mission-critical services before client-centric action items. Rapidiously recaptiualize multifunctional bandwidth and resource maximizing ideas. Assertively customize interoperable ROI and granular experiences. Holisticly integrate accurate Globally actualize mission-critical services before client-centric action items.",
-      title: "CEO otis",
-      name: "Stella Alvarez",
-    },
-    {
-      image: instructiveImage,
-      text: "Globally actualize mission-critical services before client-centric action items. Rapidiously recaptiualize multifunctional bandwidth and resource maximizing ideas. Assertively customize interoperable ROI and granular experiences. Holisticly integrate accurate Globally actualize mission-critical services before client-centric action items.",
-      title: "CEO otis",
-      name: "Stella Alvarez",
-    },
+    
   ];
   return (
     <div className={styles.testimonialsWrapper}>
       <div className={styles.bgImage}>
-        <Image src={backgroundImage} alt="image" />
+
+        <Image className="" src={backgroundImage} alt="image" />
       </div>
       <div className="container">
         <div className="flex">
@@ -135,21 +98,22 @@ const Testimonials = () => {
             {TESTIMONIALS_DATA.map((data, index) => {
               return (
                 <SwiperSlide key={index}>
-                  <Card className={styles.TestimonialCard}>
-                    <div className="flex justify-between items-center">
-                      <Image src={data.image} alt={data.title} />
+                  <Card className={`${styles.TestimonialCard} hover:bg-black hover:text-white transition duration-500 ease-in-out" hover:shadow-lg`}>
+                    <div className="h-36 flex justify-between items-center">
+                      <Image className={`w-48 ${styles.simple_logo}`} src={data.image} alt={data.title} />
+                      <Image className={` ${styles.white_logo}`} src={data.whiteimage} alt={data.title} />
                       <div className="cursor-pointer">
                         <ArrowRightBlack />
                       </div>
                     </div>
                     <div className="mt-[29px]">
-                      <p className="text-lg font-poppins font-normal text-grey mb-[24px]">
-                        {data.text}
+                      <p className={`text-lg font-poppins font-normal text-grey mb-[24px] ${styles.testmonialpara}`}>
+                        {data.text.substring(0, 299)} ...
                       </p>
-                      <a className="text-purple text-lg font-poppins font-semibold">
+                      <a className={`text-purple text-lg font-poppins font-semibold ${styles.siteName}`}>
                         {data.title}
                       </a>
-                      <p className="text-primary-dark text-lg font-poppins font-normal mt-[4px]">
+                      <p className={`text-primary-dark text-lg font-poppins font-normal mt-[4px] ${styles.siteowner}`}>
                         {data.name}
                       </p>
                     </div>
