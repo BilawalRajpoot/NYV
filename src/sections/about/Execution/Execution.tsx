27b6efc50aadb04f65a33d69fa4b42111ceed040
Bilawal ali <bilawal.ali2626@gmail.com>
2023-12-12 05:53:23 -0800

    content update

--- src/sections/about/Execution/Execution.tsx
+++ src/sections/about/Execution/Execution.tsx
@@ -2,17 +2,17 @@ import GeneralHeading from '@/components/Heading/GeneralHeading';
 import SubHeading from '@/components/Heading/SubHeading';
 import React from 'react'
 import styles from './Execution.module.css';
-import executionImage from '../../../../src/images/about/execution.png'
+import executionImage from '../../../../src/images/about/orgonogram.png'
 import Image from 'next/image';
-const heading = "We’re­ a­ full­ service­ creative­ and­ digital­ agency,­ working­ globally­ with­ largest­brands."
+const heading = "The Crucial Role of Organograms in Visualizing Organizational Structures."
 export const Execution = () => {
     return (
         <div className={styles.executuonWrapper}>
             <div className='container'>
-                <SubHeading className={'text-left mb-[8px]'} subHeading={'From ideation to execution'} />
+                <SubHeading className={'text-left mb-[8px]'} subHeading={'Navigating Organizational Structure'} />
                 <GeneralHeading className={'text-left max-w-[900px] leading-[64px] mb-[32px]'} heading={heading} />
                 <div>
-                    <Image src={executionImage} alt="execution image" />
+                    <Image className='w-full' src={executionImage} alt="execution image" />
                 </div>
             </div>
 
